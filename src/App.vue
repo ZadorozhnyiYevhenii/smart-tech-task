97b8a66527a8d45ca99b172ae9db296d38079c54
Yevhenii <zheki2004@gmail.com>
2024-01-27 15:00:19 +0200

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,103 @@
-<script setup>
+<script>
 import UserList from './components/UserList.vue';
+import UserDetails from './components/UserDetails.vue';
+import FormAddUser from './components/FormAddUser.vue';
+import CustomSelect from './components/CustomSelect.vue';
+import { fetchUsers } from './api/helpers/getUsers';
+import { addUser } from './api/helpers/addUser';
+
+export default {
+  data() {
+    return {
+      users: [],
+      newUser: {
+        name: '',
+        email: '',
+      },
+      searchQuery: '',
+      selectedSort: '',
+      sortOptions: [
+        { value: 'first_name', name: 'Sort by name' },
+        { value: 'email', name: 'Sort by email' }
+      ],
+      isUserDetailsVisible: false,
+      selectedUser: null
+    };
+  },
+  methods: {
+    async fetchUsers() {
+      try {
+        this.users = await fetchUsers();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async addUser() {
+      try {
+        const response = await addUser(this.newUser);
+        this.users.push(response);
+        this.clearForm();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    clearForm() {
+      this.newUser.email = '';
+      this.newUser.first_name = '';
+    },
+    handleUserDelete(deletedUserId) {
+      this.users = this.users.filter(user => user.id !== deletedUserId);
+    },
+    showUserDetails(user) {
+      this.selectedUser = user;
+      this.isUserDetailsVisible = true;
+    },
+    closeUserDetails() {
+      this.selectedUser = null;
+      this.isUserDetailsVisible = false;
+    }
+  },
+  components: { UserDetails, CustomSelect, FormAddUser, UserList },
+  mounted() {
+    this.fetchUsers();
+  },
+  computed: {
+    sortedUsers() {
+      return [...this.users].sort((user1, user2) => user1[this.selectedSort]?.localeCompare(user2[this.selectedSort]));
+    },
+    sortedAndSearchProducts() {
+      return this.sortedUsers.filter(user => user.first_name.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    }
+  },
+};
 </script>
 
 <template>
   <div>
-    <UserList />
+    <div class="user-top">
+      <CustomSelect 
+        :options="sortOptions"
+        v-bind:modelValue="selectedSort"
+        v-on:update:modelValue="selectedSort = $event" 
+      />
+      <input type="text" v-model="searchQuery" placeholder="Search user by name" class="user-top__input">
+    </div>
+
+    <UserList 
+    :users="sortedAndSearchProducts" 
+    :handleUserDelete="handleUserDelete"
+    :showUserDetails="showUserDetails"
+    />
+
+    <UserDetails 
+      :user="selectedUser" 
+      :isVisible="isUserDetailsVisible" 
+      @close="closeUserDetails" 
+    />
+
+    <FormAddUser 
+      :newUser="newUser" 
+      @addUser="addUser" 
+    />
   </div>
 </template>
-
-<style scoped>
-
-</style>
--- src/components/UserList.vue
+++ src/components/UserList.vue
@@ -1,107 +1,31 @@
 <script>
 import UserItem from './UserItem.vue';
-import UserDetails from './UserDetails.vue';
-import CustomSelect from './CustomSelect.vue';
-import { fetchUsers } from './../api/helpers/getUsers';
-import { addUser } from './../api/helpers/addUser'
-import FormAddUser from './FormAddUser.vue';
 
 export default {
-  data() {
-    return {
-      users: [],
-      newUser: {
-        name: '',
-        email: '',
-      },
-      searchQuery: '',
-      selectedSort: '',
-      sortOptions: [
-        { value: 'first_name', name: 'Sort by name' },
-        { value: 'email', name: 'Sort by email' }
-      ],
-      isUserDetailsVisible: false,
-      selectedUser: null
-    };
+  props: {
+    users: {
+      type: Array,
+      default: () => [],
+    },
+    handleUserDelete: Function,
+    showUserDetails: Function,
   },
-  methods: {
-    async fetchUsers() {
-      try {
-        this.users = await fetchUsers();
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    async addUser() {
-      try {
-        const response = await addUser(this.newUser);
-        this.users.push(response);
-        this.clearForm();
-      } catch (error) {
-        console.error(error);
-      }
-    },
-    clearForm() {
-      this.newUser.email = '';
-      this.newUser.first_name = '';
-    },
-    handleUserDelete(deletedUserId) {
-      this.users = this.users.filter(user => user.id !== deletedUserId);
-    },
-    showUserDetails(user) {
-      this.selectedUser = user;
-      this.isUserDetailsVisible = true;
-    },
-    closeUserDetails() {
-      this.selectedUser = null;
-      this.isUserDetailsVisible = false;
-    }
-  },
-  components: { UserItem, UserDetails, CustomSelect, FormAddUser },
-  mounted() {
-    this.fetchUsers();
-  },
-  computed: {
-    sortedUsers() {
-      return [...this.users].sort((user1, user2) => user1[this.selectedSort]?.localeCompare(user2[this.selectedSort]));
-    },
-    sortedAndSearchProducts() {
-      return this.sortedUsers.filter(user => user.first_name.toLowerCase().includes(this.searchQuery.toLowerCase()))
-    }
-  },
+  components: { UserItem }
 };
 </script>
 
 <template>
-  <div class="user-top">
-    <CustomSelect 
-      :options="sortOptions"
-      v-bind:modelValue="selectedSort"
-      v-on:update:modelValue="selectedSort = $event" 
-    />
-    <input type="text" v-model="searchQuery" placeholder="Search user by name" class="user-top__input">
-  </div>
 
   <div class="user__list">
     <UserItem 
-      v-for="user in sortedAndSearchProducts" 
+      v-for="user in users" 
       :key="user.id" 
       :user="user"
-      @userDeleted="handleUserDelete"
-      @userSelected="showUserDetails"
+      @userDeleted="handleUserDelete(user.id)"
+      @userSelected="showUserDetails(user)"
     />
   </div>
 
-  <UserDetails 
-    :user="selectedUser" 
-    :isVisible="isUserDetailsVisible" 
-    @close="closeUserDetails" 
-  />
-
-  <FormAddUser 
-    :newUser="newUser" 
-    @addUser="addUser" 
-  />
 </template>
 
 <style lang="scss">
